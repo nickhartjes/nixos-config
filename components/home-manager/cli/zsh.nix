2f0ce5ee160f23cdfc33bea482350a87862c3ce8
Nick Hartjes <nick@hartj.es>
2025-06-24 14:47:39 +0200

    feat: Update zsh configuration with additional plugins and improved SSH agent handling

--- components/home-manager/cli/zsh.nix
+++ components/home-manager/cli/zsh.nix
@@ -16,68 +16,85 @@ in {
       antidote = {
         enable = true;
         plugins = [
-          "zsh-users/zsh-syntax-highlighting"
           "zsh-users/zsh-autosuggestions"
           "zsh-users/zsh-completions"
           "romkatv/powerlevel10k"
           "Aloxaf/fzf-tab"
+          "zsh-users/zsh-syntax-highlighting"
+          # Optional modern plugin (faster highlighting):
+          # "zdharma-continuum/fast-syntax-highlighting"
+          # Optional advanced autocomplete:
+          # "marlonrichert/zsh-autocomplete"
         ];
       };
+
       shellAliases = {
         "..." = "cd ../..";
         ".." = "cd ..";
         grep = "rg";
         htop = "btop";
         ps = "procs";
-        # Security
         audit = "sudo lynis audit system";
-
-        # Shutdown or reboot
         ssn = "sudo shutdown now";
         sr = "sudo reboot";
-
-        # Information
         myip = "curl http://ipecho.net/plain; echo";
         speed = "speedtest-cli --simple";
-
-        # Clipboard (cross-platform commands)
         pbcopy = "wl-copy || xsel --clipboard --input";
         pbpaste = "wl-paste || xsel --clipboard --output";
       };
-      initContent = let
-        zshEarlyInit = lib.mkOrder 500 ''
-          export NIX_PATH=nixpkgs=channel:nixos-unstable
-          export NIX_LOG=info
 
-          # SSH Agent configuration - respect existing SSH_AUTH_SOCK
-          if [ -z "$SSH_AUTH_SOCK" ]; then
-            # Try to find ssh-agent socket in standard locations
-            if [ -S "$XDG_RUNTIME_DIR/ssh-agent" ]; then
-              export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent"
-            elif [ -S "/run/user/$(id -u)/ssh-agent" ]; then
-              export SSH_AUTH_SOCK="/run/user/$(id -u)/ssh-agent"
-            fi
+      initExtra = ''
+        # SSH Agent fallback
+        if [ -z "$SSH_AUTH_SOCK" ]; then
+          if [ -S "$XDG_RUNTIME_DIR/ssh-agent" ]; then
+            export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent"
+          elif [ -S "/run/user/$(id -u)/ssh-agent" ]; then
+            export SSH_AUTH_SOCK="/run/user/$(id -u)/ssh-agent"
           fi
-        '';
-        zshGeneralConfig = lib.mkOrder 1000 ''
-          # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-          [[ ! -f ~/.config/.p10k.zsh ]] || source ~/.config/.p10k.zsh
-          fastfetch
-        '';
-      in
-        lib.mkMerge [zshEarlyInit zshGeneralConfig];
+        fi
+
+        # History configuration
+        HISTSIZE=5000
+        HISTFILE="$HOME/.zsh_history"
+        SAVEHIST=$HISTSIZE
+        setopt appendhistory
+        setopt sharehistory
+        setopt hist_ignore_space
+        setopt hist_ignore_all_dups
+        setopt hist_save_no_dups
+        setopt hist_ignore_dups
+        setopt hist_find_no_dups
+
+        # Completion improvements
+        zstyle ':completion:*' menu select
+        zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+        zstyle ':completion:*' list-colors "${s.:. LS_COLORS}"
+
+        # fzf-tab preview configuration
+        zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+        zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+
+        # fzf shell integration
+        eval "$(fzf --zsh)"
+
+        # Source p10k config
+        [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+        fastfetch
+      '';
     };
 
-    # Applications needed for the zsh configuration to work properly
     home.packages = with pkgs; [
       btop
       procs
-      curl # For myip alias
-      speedtest-cli # For speed alias
-      xsel # For X11 clipboard
-      wl-clipboard # For Wayland clipboard
+      curl
+      speedtest-cli
+      xsel
+      wl-clipboard
+      fzf # Added for full integration
+      ripgrep # used by alias 'grep=rg'
     ];
 
-    home.file.".config/.p10k.zsh".source = ./files/.p10k.zsh;
+    # Powerlevel10k config
+    home.file.".p10k.zsh".source = ./files/.p10k.zsh;
   };
 }
