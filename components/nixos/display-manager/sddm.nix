664b3ff68b64a0b019036572ab7fc24888c772f4
Nick Hartjes <nick@hartj.es>
2026-07-28 11:41:11 +0200

    feat(display): default SDDM greeter to Wayland when enabled

--- components/nixos/display-manager/sddm.nix
+++ components/nixos/display-manager/sddm.nix
@@ -1,5 +1,15 @@
-{lib, ...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   options.components.display.sddm = {
     enable = lib.mkEnableOption "SDDM display manager";
+  };
+
+  config = lib.mkIf config.components.display.sddm.enable {
+    # Run SDDM greeter on Wayland instead spawning an X server.
+    # mkDefault so host can override back X11 if needed.
+    services.displayManager.sddm.wayland.enable = lib.mkDefault true;
   };
 }
